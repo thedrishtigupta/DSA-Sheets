8072d67ef43743158359a232dcf867b8168c275e
Drishti Gupta <thedrishtigupta@gmail.com>
2026-07-08 19:45:24 +0530

    NC 150: Hashing - Contains duplicate

---
+++ NeetCode_150/217_Contains_duplicate.cpp
@@ -0,0 +1,15 @@
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for(int val : nums) {
+            if(s.count(val)) return true;
+            s.insert(val);
+        }
+
+        return false;
+    }
+};
